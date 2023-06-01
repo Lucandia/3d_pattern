13d32c112a54ee966e33c539360a52766cb767e4
lmonari5 <monarluca@gmail.com>
2023-06-01 12:14:22 +0200

    fixing openscad preview on server: -no listen unix

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,10 +66,6 @@ if __name__ == "__main__":
     st.title('Soap Dish 3D Pattern')
     st.write('Generate a 3D model for a custom soap dish! You can find more information about the soap dish here on [Printables](https://www.printables.com/it/model/489136-geometric-soap-dish-holder-normal-with-plate-or-or).')
     cwd = os.getcwd() + os.sep
-    # Get sudo priviledge
-    subprocess.run(['cat “12345” | sudo tee -a /etc/machine-id',
-                    'cat “12345” | sudo tee -a /var/lib/dbus/machine-id'],
-                    shell=True)
     for file in os.listdir():
         if 'file.' in file:
            os.remove(file)        
@@ -123,7 +119,7 @@ if __name__ == "__main__":
         # run openscad
         with st.spinner('Rendering in progress...'):
             if preview:
-                subprocess.run(['Xvfb :99 & export DISPLAY=:99',
+                subprocess.run(['Xvfb :99 & export DISPLAY=:99 -nolisten unix',
                                 'b=`basename preview`',
                                 f'openscad -o $b.png {run_file}'],
                                 shell = True)
